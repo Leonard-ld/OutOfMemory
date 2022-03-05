3dfc026856b236eddc232c9c524087958714cccb
Leonard-ld <1259535860@qq.com>
2022-03-05 21:24:41 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# 工程简介
+SpringBoot: 2.3.7.RELEASE
+# 延伸阅读
+
---
+++ src/test/java/developer/outofmemory/OutOfMemoryApplicationTests.java
@@ -0,0 +1,13 @@
+package developer.outofmemory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OutOfMemoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
